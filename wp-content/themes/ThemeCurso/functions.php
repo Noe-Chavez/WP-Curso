f15de6c6aca2018e1da3ae5f95daa0a57463d25b
Noe-Chavez <noe_fam_chavez@yahoo.com.mx>
2021-01-28 21:23:05 -0600

    Estilo boostrap y fuente de letra, asociación de css

--- wp-content/themes/ThemeCurso/functions.php
+++ wp-content/themes/ThemeCurso/functions.php
@@ -11,6 +11,18 @@
      * primer argumento: Cuando alguén ingrese al sitio, WP eliga el tema y ejecute nuestras opciones.
      * segundo argumento: Es el nombre de la función.
      */
-    add_action( 'after_setup_theme', 'init_template')
+    add_action( 'after_setup_theme', 'init_template');
+
+    function assets() {
+        # Establecer dependencia a bootstrap para estilo.
+        wp_register_style('bootstrap','https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css','','5.0.0','all');
+        # Estabaler depednencia de google, tipo de letra moncerrat.
+        wp_register_style('montserrat','https://fonts.googleapis.com/css2?family=Montserrat&display=swap','','1.0','all');
+        # Registrar en WP las dos dependencia anteriores y el archivo style.css (utilizando la función get_stylesheet_uri() que nos devuelve la ruta de éste).
+        wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap','montserrat'),'1.0','all'); 
+    }
+
+    # Agregar al hook, cuando se cargé lapágina (wp_enqueue_scripts), la función assets que contienen el tipo de letra y los estilos. 
+    add_action('wp_enqueue_scripts','assets');
 
 ?>
