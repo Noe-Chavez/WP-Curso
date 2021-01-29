474c820186c75cbba88342a180dec740ae104e81
Noe-Chavez <noe_fam_chavez@yahoo.com.mx>
2021-01-28 22:01:16 -0600

    Vinculando script propios y de boostrap, así como establcer logo

--- wp-content/themes/ThemeCurso/functions.php
+++ wp-content/themes/ThemeCurso/functions.php
@@ -20,9 +20,27 @@
         wp_register_style('montserrat','https://fonts.googleapis.com/css2?family=Montserrat&display=swap','','1.0','all');
         # Registrar en WP las dos dependencia anteriores y el archivo style.css (utilizando la función get_stylesheet_uri() que nos devuelve la ruta de éste).
         wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap','montserrat'),'1.0','all'); 
+
+        #Agregando JQuery y popper, dependencia que requiere bootstrap para funcionar.
+        wp_register_script( 'popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js', '', '2.5.4', true );
+        
+        # Agregar en cola las dependeicias de jascropt para boostrap. 
+        #Nota: JQuery ya está defenida en wp, por lo tanto sólo hay que encolar.
+        wp_enqueue_script( 'boostraps', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js', array('jquery', 'popper'), '5.0.0', true);
+
+        #Poner en cola nuestro propio arichivo js (assets-->js-->custom.js)
+        /**
+         *  get_template_directory_uri(): obtiene la ubicación de nuestro template y le concatemanos la ruta
+         *  en donde se encuentra nuestro custom.js. Con el objetivo que sea de manera diná,mica la ruta.
+         */
+        wp_enqueue_script( 'custom', get_template_directory_uri().'/assets/js/custom.js', '', '1.0', true);
+
     }
 
     # Agregar al hook, cuando se cargé lapágina (wp_enqueue_scripts), la función assets que contienen el tipo de letra y los estilos. 
     add_action('wp_enqueue_scripts','assets');
+
+
+
 
 ?>
--- wp-content/themes/ThemeCurso/header.php
+++ wp-content/themes/ThemeCurso/header.php
@@ -8,3 +8,17 @@
     <?php wp_head(); ?>
 </head>
 <body>
+
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+            <!-- 
+                se establece de manera dinamica la ruta del logo, para ello se utiliza get_template_directory_uri()
+                Para establecer de manera dinamica la raíz.
+             -->
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/logo.png" alt="logo" >
+            </div>
+        </div>
+    </div>
+</header>
